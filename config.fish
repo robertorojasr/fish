82367676fc0ce778f47c50a6f8104b208ce7841d
Roberto R <roberto.rojas@gmail.com>
2015-09-03 10:11:42 -0300

    Ajustes de T420

--- config.fish
+++ config.fish
@@ -1,4 +1,6 @@
 
-eval (keychain --eval --agents ssh -Q --quiet local_rsa)
+eval (keychain --eval --agents ssh -Q --quiet id_rsa)
+set PATH /home/rob/.gem/ruby/2.2.0/bin/ $PATH
 
 set fish_greeting
+
